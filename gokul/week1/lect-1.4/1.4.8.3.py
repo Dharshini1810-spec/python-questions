0a04ef49353449f4701e396c7fcf2e128d208d19
Dharshini S <dharshinis0610@outlook.com>
2026-06-23 11:44:30 +0530

    datatype conversion

--- gokul/week1/lect-1.4/1.4.8.3.py
+++ gokul/week1/lect-1.4/1.4.8.3.py
@@ -4,10 +4,16 @@ print(type(x))
 
 y="10.5"
 # convert y's datatype to  float
-
+y=float(y)
+print(type(y))
 
 z=10.5
 # what is the datatype of z?
+print(type(z))
 
 
 # convert z to an integer
+z=int(input("enter the num:"))
+print(type(z))
+
+
