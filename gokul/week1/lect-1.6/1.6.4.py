080d0f8046538231cde4b07afc3c5283a84d9758
Dharshini S <dharshinis0610@outlook.com>
2026-06-26 11:50:39 +0530

    radius and area

--- gokul/week1/lect-1.6/1.6.4.py
+++ gokul/week1/lect-1.6/1.6.4.py
@@ -1,1 +1,4 @@
 # 1. write a python code that takes radius of circle as input and prints the area of the circle
+r=int(input("the radius is:"))
+area=3.14*r*r
+print(f"the area of circle of radius {r} is {area}")
